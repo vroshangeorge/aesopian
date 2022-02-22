0c8b1b8b6ea926715879694119c09b584bf01b63
vroshangeorge <46734101+vrgeorge0047@users.noreply.github.com>
2022-02-22 23:42:31 +0530

    Auto-generated commit [ci skip]

--- src/app/shared/directives/turnjs.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TurnjsDirective } from './turnjs.directive';
-
-describe('TurnjsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TurnjsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
